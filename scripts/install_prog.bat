763c12329627b532c26f8d7fe88c0f6a0c5230bb
Jacob Meredith <jacobmeredith@tuta.io>
2021-09-05 16:19:18 -0400

    Adding a couple new scripts

---
+++ scripts/install_prog.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: chocolatey
+autohotkey
+brave
+bulk-crap-uninstaller
+ditto
+ds4windows
+foobar2000
+gpg4win
+jbs
+libreoffice-fresh
+msiafterburner
+notepadplusplus
+powertoys
+qtpass
+teracopy
+tinywall
+transmission
+ums
+vcxsrv
+winaero-tweaker
+
+:: scoop
+cmder-full
+exiftool
+gsudo
+nvcleanstall
+openjdk
---
+++ scripts/itsmine.cmd
@@ -0,0 +1,3 @@
+
+takeown /f %1 /r /d y
+icacls %1 /grant administrators:F /t
