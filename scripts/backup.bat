7a5b9e2cdebf9c618f4898517c104db51e1bc941
Jacob Meredith <jacobmeredith@tuta.io>
2021-09-06 15:38:08 -0400

    Adding new backup script Rewrote my old backup script.  Not fully complete, but the basic functionality is there

---
+++ scripts/backup.bat
@@ -0,0 +1,63 @@
+@echo off
+set "backupdir=D:\Backups"
+
+if "%1" == "" (
+	goto:help
+	exit /b 0
+)
+if "%1" == "docs" (
+	call:docs
+	exit /b 0
+)
+if "%1" == "wsl" (
+	call:wsl
+	exit /b 0
+) 
+if "%1" == "settings" (
+	call:settings
+	exit /b 0
+)
+if "%1" == "all" (
+	goto:docs
+	goto:wsl
+	goto:settings
+	exit /b 0
+) else (
+	echo Invalid command.
+	goto:help
+	exit /b 0
+)
+
+:: Document and tools backup
+:docs
+ROBOCOPY.EXE S:\documents\ %backupdir%\docs-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE S:\documents-serena\ %backupdir%\docs-serena /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE X:\shared-documents\ %backupdir%\docs-shared /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE S:\tools\ %backupdir%\tools-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+exit /b 0
+
+
+:: wsl backup
+:wsl
+echo Backing up Arch wsl install...
+wsl --export Arch X:\archive-jake\Backups\wsl\wsl_arch.tar.gz
+echo Done.
+exit /b 0
+
+
+:: Settings backup
+:settings
+7z u -up1q0r2x1y2z1w2 -mx9 -mmt16 %backupdir%\settings-jake\my_settings.7z X:\archive-jake\my-settings
+exit /b 0
+
+
+:: Help Section
+:help
+echo Usage: backup (command) [flags]
+echo Backup important files to external media.
+echo[
+echo docs		Backup documents
+echo wsl		Backup wsl
+echo settings	Backup settings (windows, linux, dotfiles...)
+echo all		All of the above
+exit /b 0
