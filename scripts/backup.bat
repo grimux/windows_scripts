13ca95656ba9b55665fd2f4fa24b3804acfb2613
Jacob Meredith <jacobmeredith@tuta.io>
2021-09-18 22:09:09 -0400

    Added some spacing

--- scripts/backup.bat
+++ scripts/backup.bat
@@ -60,44 +60,79 @@ if "%1" == "all" (
 
 :: Document and tools backup
 :docs
+echo ====================================================================
+echo Backing up documents...
+echo --------------------------------------------------------------------
 ROBOCOPY.EXE S:\documents\ %backupdir%\docs-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
 ROBOCOPY.EXE S:\documents-serena\ %backupdir%\docs-serena /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
 ROBOCOPY.EXE X:\shared-documents\ %backupdir%\docs-shared /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
 ROBOCOPY.EXE S:\tools\ %backupdir%\tools-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+echo[
+echo Done!
+echo ====================================================================
 exit /b 0
 
 
 :: wsl backup
 :wsl
+echo ====================================================================
 echo Backing up Arch wsl install...
+echo --------------------------------------------------------------------
 wsl --export Arch X:\archive-jake\Backups\wsl\wsl_arch.tar.gz
-echo Done.
+echo[
+echo Done!
+echo ====================================================================
 exit /b 0
 
 
 :: Settings backup
 :settings
+echo ====================================================================
+echo Backing up settings...
+echo --------------------------------------------------------------------
 7z u -up1q0r2x1y2z1w2 -mx9 -mmt16 %backupdir%\settings-jake\my_settings.7z X:\archive-jake\my-settings
+echo[
+echo Done!
+echo ====================================================================
 exit /b 0
 
 
 :: Screenshots and wallpapers
 :screenshots
+echo ====================================================================
+echo Backing up screenshots and wallpapers...
+echo --------------------------------------------------------------------
 ROBOCOPY.EXE S:\game-screenshots\ %backupdir%\game-screenshots-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
 ROBOCOPY.EXE S:\wallpapers\ %backupdir%\wallpapers-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
 ROBOCOPY.EXE S:\screenshots\ %backupdir%\screenshots-jake /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+echo[
+echo Done!
+echo ====================================================================
 exit /b 0
 
 
 :: Pictures
 :pictures
+echo ====================================================================
+echo Backing up pictures...
+echo --------------------------------------------------------------------
 ROBOCOPY.EXE X:\Pictures\ %backupdir%\pictures /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+echo[
+echo Done!
+echo ====================================================================
 exit /b 0
 
 
 :: Music
 :music
+echo ====================================================================
+echo Backing up music...
+echo --------------------------------------------------------------------
 ROBOCOPY.EXE S:\music\ %backupdir%\music /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+echo[
+echo Done!
+echo ====================================================================
+exit /b 0
 
 :: Help Section
 :help
