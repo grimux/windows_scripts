3e6bc6ff2ce9ee47f8ac5d89e2735b1744f599ca
Jacob Meredith <jacobmeredith@tuta.io>
2021-09-26 10:52:21 -0400

    Added check for existing "vcxsrv.exe" task Script will now check for running instance, and kill it

--- scripts/startx.bat
+++ scripts/startx.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: x-server
+set server="vcxsrv.exe"
+
+:: Check if vcxsrv is running
+tasklist | findstr /I %server%
+REM errorlevel of 0 means the server is already running, so kill it
+if "%ERRORLEVEL%" == "0" call toggle-vcxsrv.cmd
+
 :: Run the config for VcXsrv
 "S:\tools\vcxsrv\fullscreen.xlaunch"
 
