47345212bdf3ce6db2016f14cff68675aa019908
Jacob Meredith <jacobmeredith@tuta.io>
2021-09-26 10:53:18 -0400

    Rewrote check for "vcxsrv.exe" task

--- scripts/toggle-vcxsrv.cmd
+++ scripts/toggle-vcxsrv.cmd
@@ -1,14 +1,16 @@
 @echo off
 :: This script is for toggling the xserver for wsl
 
+set server="vcxsrv.exe"
+
 :: Search for the vcxsrv instance, either start or kill
-tasklist /fi "ImageName eq vcxsrv.exe" /fo csv 2>NUL | find /I "vcxsrv.exe">NUL
-if "%ERRORLEVEL%"=="0" (
+tasklist | findstr /I %server%
+if "%ERRORLEVEL%" == "0" (
 	echo vcxsrv is running, killing...
-	taskkill /f /im vcxsrv.exe
+	taskkill /f /im %server%
 	S:\tools\SystemTrayRefresh.exe
 )
-if "%ERRORLEVEL%"=="1" (
-	echo starting vcxsrv...
+if "%ERRORLEVEL%" == "1" (
+	echo starting %server%...
 	S:\tools\vcxsrv\config.xlaunch
 )
