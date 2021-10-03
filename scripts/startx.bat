25207e9952a393a0972dc3d00c19645e530a20ad
Jacob Meredith <jacobmeredith@tuta.io>
2021-10-03 09:10:33 -0400

    Added pulseaudio to startup script WSL will now have audio

--- scripts/startx.bat
+++ scripts/startx.bat
@@ -11,5 +11,11 @@ if "%ERRORLEVEL%" == "0" call toggle-vcxsrv.cmd
 :: Run the config for VcXsrv
 "S:\tools\vcxsrv\fullscreen.xlaunch"
 
+:: Start Puleseadio
+start "" /B "C:\wsl\pulseaudio\bin\pulseaudio.exe"
+
 :: Run my win-desk script inside wsl
 wsl win-desk
+
+:: Kill pulseaudio after server is closed
+taskkill /IM pulseaudio.exe /F
