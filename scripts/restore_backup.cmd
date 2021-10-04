e2cb9503798a2627d83111fd97a7ffdc91115d0b
Jacob Meredith <jacobmeredith@tuta.io>
2021-10-04 18:12:38 -0400

    Restoration script for backup drive. Only use this is moving files to an empty drive.

---
+++ scripts/restore_backup.cmd
@@ -0,0 +1,6 @@
+ROBOCOPY.EXE D:\Backups\docs-jake\ S:\documents /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE D:\Backups\docs-serena\ S:\documents-serena /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE D:\Backups\music\ S:\music /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE D:\Backups\screenshots-jake\ S:\screenshots /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE D:\Backups\wallpapers-jake\ S:\wallpapers /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
+ROBOCOPY.EXE D:\Backups\game-screenshots-jake\ S:\game-screenshots /E /DCOPY:DAT /MIR /XA:S /R:1 /W:3 /MT:16
