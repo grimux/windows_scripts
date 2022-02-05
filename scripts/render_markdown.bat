79926cb1911d9dc66ccb0e79e16f858380ee8f8b
Jacob Meredith <jacobmeredith@tuta.io>
2022-02-05 10:58:31 -0500

    Added some more comments

--- scripts/render_markdown.bat
+++ scripts/render_markdown.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem this script is meant to be run from Notepad++, to easily render markdown files into pdfs
+
 rem %~1 argument is full path to file
 rem %~2 argument is path to file, minus extension
 rem run command for notepad++ is "S:\tools\scripts\render_markdown.bat $(FULL_CURRENT_PATH) $(CURRENT_DIRECTORY)\$(NAME_PART)"
