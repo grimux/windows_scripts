fcc556c34e455f539525687729717fb970adb91e
Jacob Meredith <jacobmeredith@tuta.io>
2022-02-05 10:56:05 -0500

    Added some comments

--- scripts/render_markdown.bat
+++ scripts/render_markdown.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem %~1 argument is full path to file
+rem %~2 argument is path to file, minus extension
+rem run command for notepad++ is "S:\tools\scripts\render_markdown.bat $(FULL_CURRENT_PATH) $(CURRENT_DIRECTORY)\$(NAME_PART)"
+rem remember to delete pdf files if run by accident
 
-wsl -d Arch -- pandoc $(wslpath "%~1") -o $(wslpath "%~1.pdf")
-start /b SumatraPDF.exe "%~1.pdf"
+wsl -d Arch -- pandoc $(wslpath "%~1") -o $(wslpath "%~2.pdf")
+start /b SumatraPDF.exe "%~2.pdf"
